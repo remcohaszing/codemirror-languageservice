ed99816bb3d9c97a551224177024a853d7fa600d
Remco Haszing <remcohaszing@gmail.com>
2024-09-05 13:56:23 +0200

    Fix formatting issues and update example code

--- example/scripts/typescript.ts
+++ example/scripts/typescript.ts
@@ -14,7 +14,7 @@ import {
   type ProjectContext,
   type SourceScript
 } from '@volar/language-service'
-import { createLanguageServiceHost, createSys, resolveFileLanguageId as resolveTsFileLanguageId } from '@volar/typescript'
+import { createLanguageServiceHost, createSys, resolveFileLanguageId } from '@volar/typescript'
 import {
   createCompletionSource,
   createHoverTooltipSource,
@@ -46,6 +46,157 @@ function markdownToDom(markdown: string): Node {
   return html
 }
 
+const docUri = 'file:///example.tsx'
+const docText = `import { ChangeEventHandler, ReactNode, useState } from 'react'
+
+export namespace Greeting {
+  export interface Props {
+    /**
+     * The name of the person to greet.
+     */
+    name: string
+  }
+}
+
+/**
+ * Render a greeting for a person.
+ */
+export function Greeting({ name }: Greeting.Props): ReactNode {
+  console.log('Hello', \`$\{name}!\`)
+
+  return (
+    <div>
+      Hello <strong>{name}</strong>!
+    </div>
+  )
+}
+
+/**
+ * Render the app.
+ */
+export function App(): ReactNode {
+  const [name, setName] = useState('Volar')
+
+  const handleChange: ChangeEventHandler = (event) => {
+    setName(event.currentTarget.name)
+  }
+
+  return (
+    <div>
+      <input defaultValue={name} onChange={handleChange} />
+      <Greeting name={name} />
+    </div>
+  )
+}
+`
+
+const env: LanguageServiceEnvironment = {
+  fs: createNpmFileSystem(),
+  workspaceFolders: []
+}
+const uriConverter = {
+  asUri: URI.file,
+  asFileName(uri: URI) {
+    return uri.path
+  }
+}
+const sys = createSys(ts.sys, env, () => '', uriConverter)
+const syncDocuments =
+  createUriMap<[TextDocument, number | undefined, ts.IScriptSnapshot | undefined]>()
+const fsFileSnapshots = createUriMap<[number | undefined, ts.IScriptSnapshot | undefined]>()
+const language = createLanguage(
+  [
+    {
+      getLanguageId: (uri) => syncDocuments.get(uri)?.[0].languageId
+    },
+    {
+      getLanguageId: (uri) => resolveFileLanguageId(uri.path)
+    }
+  ],
+  createUriMap<SourceScript<URI>>(false),
+  (uri, includeFsFiles) => {
+    let snapshot: ts.IScriptSnapshot | undefined
+
+    const syncDocument = syncDocuments.get(uri)
+    if (syncDocument) {
+      if (!syncDocument[2] || syncDocument[0].version !== syncDocument[1]) {
+        syncDocument[1] = syncDocument[0].version
+        syncDocument[2] = ts.ScriptSnapshot.fromString(syncDocument[0].getText())
+      }
+      snapshot = syncDocument[2]
+    } else if (includeFsFiles) {
+      const cache = fsFileSnapshots.get(uri)
+      const fileName = uriConverter.asFileName(uri)
+      const modifiedTime = sys.getModifiedTime?.(fileName)?.valueOf()
+      if (!cache || cache[0] !== modifiedTime) {
+        if (sys.fileExists(fileName)) {
+          const text = sys.readFile(fileName)
+          fsFileSnapshots.set(uri, [
+            modifiedTime,
+            text === undefined ? undefined : ts.ScriptSnapshot.fromString(text)
+          ])
+        } else {
+          fsFileSnapshots.set(uri, [modifiedTime, undefined])
+        }
+      }
+      snapshot = fsFileSnapshots.get(uri)?.[1]
+    }
+
+    if (snapshot) {
+      language.scripts.set(uri, snapshot)
+    } else {
+      language.scripts.delete(uri)
+    }
+  }
+)
+const project: ProjectContext = {
+  typescript: {
+    configFileName: '',
+    sys,
+    uriConverter,
+    ...createLanguageServiceHost(ts, sys, language, URI.file, {
+      getCompilationSettings() {
+        return {
+          checkJs: true,
+          jsx: ts.JsxEmit.ReactJSX,
+          module: ts.ModuleKind.Preserve,
+          target: ts.ScriptTarget.ESNext
+        }
+      },
+      getCurrentDirectory() {
+        return sys.getCurrentDirectory()
+      },
+      getScriptFileNames() {
+        return [docUri.slice('file://'.length)]
+      }
+    })
+  }
+}
+const languageService = createLanguageService(
+  language,
+  createTypeScriptPlugins(ts, {}),
+  env,
+  project
+)
+
+/**
+ * Synchronize a document from CodeMirror into Volar.
+ *
+ * @param document
+ *   The document to synchronize.
+ * @returns
+ *   The URI that matches the document.
+ */
+function sync(document: TextDocument): URI {
+  const uri = URI.parse(document.uri)
+  if (syncDocuments.has(uri)) {
+    syncDocuments.get(uri)![0] = document
+  } else {
+    syncDocuments.set(uri, [document, undefined, undefined])
+  }
+  return uri
+}
+
 const completionOptions: createCompletionSource.Options = {
   section: 'TypeScript',
   markdownToDom,
@@ -67,14 +218,6 @@ const lintOptions: createLintSource.Options = {
     return languageService.getDiagnostics(sync(document))
   }
 }
-
-const docUri = 'file:///example.tsx'
-const docText = `import {} from 'react'
-
-console.log('hi!')
-
-foo(
-`
 
 const view = new EditorView({
   doc: docText,
@@ -102,8 +245,6 @@ const view = new EditorView({
   ]
 })
 
-const { languageService, sync } = getVolarLanguageService()
-
 document.getElementById('format-button')!.addEventListener('click', async () => {
   const document = getTextDocument(view.state)
   const text = document.getText()
@@ -121,111 +262,3 @@ document.getElementById('format-button')!.addEventListener('click', async () => 
     dispatchTextEdits(view, edits)
   }
 })
-
-function getVolarLanguageService() {
-  const env: LanguageServiceEnvironment = {
-    fs: createNpmFileSystem(),
-    workspaceFolders: []
-  }
-  const uriConverter = {
-    asUri: URI.file,
-    asFileName(uri: URI) {
-      return uri.path
-    }
-  }
-  const sys = createSys(ts.sys, env, () => '', uriConverter)
-  const syncDocuments = createUriMap<[TextDocument, number | undefined, ts.IScriptSnapshot | undefined]>()
-  const fsFileSnapshots = createUriMap<[number | undefined, ts.IScriptSnapshot | undefined]>()
-  const language = createLanguage(
-    [
-      {
-        getLanguageId: uri => syncDocuments.get(uri)?.[0].languageId
-      },
-      {
-        getLanguageId: uri => resolveTsFileLanguageId(uri.path)
-      }
-    ],
-    createUriMap<SourceScript<URI>>(false),
-    (uri, includeFsFiles) => {
-      let snapshot: ts.IScriptSnapshot | undefined
-
-      const syncDocument = syncDocuments.get(uri)
-      if (syncDocument) {
-        if (!syncDocument[2] || syncDocument[0].version !== syncDocument[1]) {
-          syncDocument[1] = syncDocument[0].version
-          syncDocument[2] = ts.ScriptSnapshot.fromString(syncDocument[0].getText())
-        }
-        snapshot = syncDocument[2]
-      }
-      else if (includeFsFiles) {
-        const cache = fsFileSnapshots.get(uri)
-        const fileName = uriConverter.asFileName(uri)
-        const modifiedTime = sys.getModifiedTime?.(fileName)?.valueOf()
-        if (!cache || cache[0] !== modifiedTime) {
-          if (sys.fileExists(fileName)) {
-            const text = sys.readFile(fileName)
-            const snapshot = text !== undefined ? ts.ScriptSnapshot.fromString(text) : undefined
-            fsFileSnapshots.set(uri, [modifiedTime, snapshot])
-          }
-          else {
-            fsFileSnapshots.set(uri, [modifiedTime, undefined])
-          }
-        }
-        snapshot = fsFileSnapshots.get(uri)?.[1]
-      }
-
-      if (snapshot) {
-        language.scripts.set(uri, snapshot)
-      }
-      else {
-        language.scripts.delete(uri)
-      }
-    }
-  )
-  const project: ProjectContext = {
-    typescript: {
-      configFileName: '',
-      sys,
-      uriConverter,
-      ...createLanguageServiceHost(ts, sys, language, URI.file, {
-        getCompilationSettings() {
-          return {
-            checkJs: true,
-            jsx: ts.JsxEmit.ReactJSX,
-            module: ts.ModuleKind.Preserve,
-            target: ts.ScriptTarget.ESNext
-          }
-        },
-        getCurrentDirectory() {
-          return sys.getCurrentDirectory()
-        },
-        getScriptFileNames() {
-          return [docUri.slice('file://'.length)]
-        }
-      })
-    }
-  }
-  return {
-    languageService: createLanguageService(language, createTypeScriptPlugins(ts, {}), env, project),
-    sync,
-  }
-
-  /**
-   * Synchronize a document from CodeMirror into Volar.
-   *
-   * @param document
-   *   The document to synchronize.
-   * @returns
-   *   The URI that matches the document.
-   */
-  function sync(document: TextDocument): URI {
-    const uri = URI.parse(document.uri)
-    if (syncDocuments.has(uri)) {
-      syncDocuments.get(uri)![0] = document
-    }
-    else {
-      syncDocuments.set(uri, [document, undefined, undefined])
-    }
-    return uri
-  }
-}
